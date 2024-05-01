8cf4b4461dd4860975e74a99692a5f918ffc8121
Ycaro02 <ycaro@Kali.home>
2024-05-01 13:37:33 -0500

    add CI

--- install/install_deps.sh
+++ install/install_deps.sh
@@ -12,9 +12,9 @@ if [ ! -d "rsc/deps/cglm" ]; then
 	./install/load_glm.sh
 fi
 
-
-# sudo apt install libwayland-dev libxkbcommon-dev xorg-dev deps needed for glfw3
-# Cmake is mandatory too
+# deps needed for glfw3
+# apt install libwayland-dev libxkbcommon-dev xorg-dev
+# Cmake mandatory too
 if [ ! -f "rsc/lib_deps/libglfw3.a" ]; then
 	./install/clone_compile_glfw3.sh
 fi
