4fdd152ce49108c4135533ed317bd0c9b67c6f13
Ycaro02 <ycaro@Kali.home>
2024-05-05 11:55:50 -0500

    Ugly frag shader to display colored chunks

--- rsc/shaders/cube_fragment_shader.glsl
+++ rsc/shaders/cube_fragment_shader.glsl
@@ -3,11 +3,21 @@
 out vec4 FragColor;
 
 in vec2 TexCoord;
+in vec2 blockPos;
 
 uniform sampler2D texture1;
 
 void main()
 {
-	// FragColor = vec4(1.0f, 0.5f, 0.5f, 1.0f);
+    if ((blockPos.x > 0.0 && blockPos.x < 16.0) && (blockPos.y > 0.0 && blockPos.y < 16.0)) {
+        FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        return ;
+    } else if ((blockPos.x > 16.0 && blockPos.x < 32.0) && (blockPos.y > 16.0 && blockPos.y < 32.0)) {
+        FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+        return ;
+    } else if ((blockPos.x > -16.0 && blockPos.x < 0.0) && (blockPos.y > -16.0 && blockPos.y < 0.0)) {
+        FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+        return ;
+    }
     FragColor = texture(texture1, TexCoord);
 }
