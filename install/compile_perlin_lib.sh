5935a33cad1c29602e5d7ac2436dfde68f557f15
Ycaro02 <Ycaro02@hotmail.fr>
2024-06-19 19:13:28 +0200

    Test create lib dir before perlin lib

--- install/compile_perlin_lib.sh
+++ install/compile_perlin_lib.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # This script is used to update the perlin noise source code in the project.
-make -s -C rsc/perlin_noise lib
-mv rsc/perlin_noise/perlin_noise.a rsc/lib_deps/libperlin_noise.a
+make -s -C rsc/perlin_noise lib && mkdir -p rsc/lib_deps && mv rsc/perlin_noise/perlin_noise.a rsc/lib_deps/libperlin_noise.a
