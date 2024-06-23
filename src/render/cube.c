72beba450161fab987ed683a1d31cc54860d4104
Ycaro02 <Ycaro02@hotmail.fr>
2024-06-23 21:24:19 +0200

    Clean cube_face modele init

--- src/render/cube.c
+++ src/render/cube.c
@@ -19,25 +19,29 @@ GLuint bufferGlCreate(GLenum type, u32 size, void *data)
 }
 
 FaceCubeModel *cubeFaceVAOinit() {
-	FaceCubeModel *cubeFace = NULL;
-    const VertexTexture backFace[] = { CUBE_BACK_FACE_VERTEX };
+    /* Initialize each face modele with texture coord */
+	const VertexTexture backFace[] = { CUBE_BACK_FACE_VERTEX };
     const VertexTexture frontFace[] = { CUBE_FRONT_FACE_VERTEX };
     const VertexTexture leftFace[] = { CUBE_LEFT_FACE_VERTEX };
     const VertexTexture rightFace[] = { CUBE_RIGHT_FACE_VERTEX };
     const VertexTexture bottomFace[] = { CUBE_BOTTOM_FACE_VERTEX };
     const VertexTexture topFace[] = { CUBE_TOP_FACE_VERTEX };
 
-    const vec3_u32 backIndices[] = { CUBE_BACK_FACE(0, 1, 2, 3) };
-    const vec3_u32 frontIndices[] = { CUBE_FRONT_FACE(0, 1, 2, 3) };
-    const vec3_u32 leftIndices[] = { CUBE_LEFT_FACE(0, 1, 2, 3) };
-    const vec3_u32 rightIndices[] = { CUBE_RIGHT_FACE(0, 1, 2, 3) };
-    const vec3_u32 bottomIndices[] = { CUBE_BOTTOM_FACE(0, 1, 2, 3) };
-    const vec3_u32 topIndices[] = { CUBE_TOP_FACE(0, 1, 2, 3) };
+	/* Initialize cube face indices */
+    const vec3_u32		backIndices[] = { CUBE_BACK_FACE(0, 1, 2, 3) };
+    const vec3_u32		frontIndices[] = { CUBE_FRONT_FACE(0, 1, 2, 3) };
+    const vec3_u32		leftIndices[] = { CUBE_LEFT_FACE(0, 1, 2, 3) };
+    const vec3_u32		rightIndices[] = { CUBE_RIGHT_FACE(0, 1, 2, 3) };
+    const vec3_u32		bottomIndices[] = { CUBE_BOTTOM_FACE(0, 1, 2, 3) };
+    const vec3_u32		topIndices[] = { CUBE_TOP_FACE(0, 1, 2, 3) };
 
-    const VertexTexture *faces[] = { backFace, frontFace, leftFace, rightFace, bottomFace, topFace };
-    const vec3_u32 *indices[] = { backIndices, frontIndices, leftIndices, rightIndices, bottomIndices, topIndices };
-    size_t vertexSizes[] = { sizeof(backFace), sizeof(frontFace), sizeof(leftFace), sizeof(rightFace), sizeof(bottomFace), sizeof(topFace) };
-    size_t indexSizes[] = { sizeof(backIndices), sizeof(frontIndices), sizeof(leftIndices), sizeof(rightIndices), sizeof(bottomIndices), sizeof(topIndices) };
+	/* Initialize cube face model */
+    const VertexTexture	*faces[] = { backFace, frontFace, leftFace, rightFace, bottomFace, topFace };
+    const vec3_u32 		*indices[] = { backIndices, frontIndices, leftIndices, rightIndices, bottomIndices, topIndices };
+    const size_t		vertexSizes[] = { sizeof(backFace), sizeof(frontFace), sizeof(leftFace), sizeof(rightFace), sizeof(bottomFace), sizeof(topFace) };
+    const size_t		indexSizes[] = { sizeof(backIndices), sizeof(frontIndices), sizeof(leftIndices), sizeof(rightIndices), sizeof(bottomIndices), sizeof(topIndices) };
+	
+	FaceCubeModel 		*cubeFace = NULL;
 
 	if (!(cubeFace = malloc(sizeof(FaceCubeModel) * FACE_VERTEX_ARRAY_SIZE))) {
 		return (NULL);
